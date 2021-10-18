6706bd14466ec6484dc896a974353d97b33b79d8
mekonensarah <90749833+mekonensarah@users.noreply.github.com>
2021-10-17 21:10:14 -0400

    Commit changes - naming conventions of 8 unit tests updated in REQ_001

--- REQ_001/REQ_001.cpp
+++ REQ_001/REQ_001.cpp
@@ -16,7 +16,7 @@ namespace REQ001
 	{
 	public:
 		
-		TEST_METHOD(getPerimeterFunction_)
+		TEST_METHOD(getPerimeterFunction_LegnthandWidth_Equal)
 		{
 			//testing perimeter function using 2 as length and 87 as width to equal 178 perimeter
 			int perimeter = 0;
@@ -26,7 +26,7 @@ namespace REQ001
 			Assert::AreEqual(178, perimeter);
 
 		}
-		TEST_METHOD(getAreaFunction_)
+		TEST_METHOD(getAreaFunction_LengthandWidth_Equal)
 		{
 			//testing area function using 4 as length and 99 as width to equal 396 area
 			int area = 0;
@@ -36,7 +36,7 @@ namespace REQ001
 			Assert::AreEqual(396, area);
 
 		}
-		TEST_METHOD(setLengthFunction_1)
+		TEST_METHOD(setLengthFunction1_LengthandInput_Equal)
 		{
 			//testing length function using length of 98 and input of 6 
 			int length = 98;
@@ -46,7 +46,7 @@ namespace REQ001
 
 		}
 
-		TEST_METHOD(setLengthFuction_2)
+		TEST_METHOD(setLengthFuction2_LengthandInput_NotSame)
 		{
 			//testing length function by using length of 44 and input of -4 which are not equal 
 			int length = 44;
@@ -56,7 +56,7 @@ namespace REQ001
 
 		}
 
-		TEST_METHOD(setLengthFunction_3)
+		TEST_METHOD(setLengthFunction3_LengthandInput_NotEqual)
 		{
 			//testing length function by using length of 198 and input of 3 which are not equal
 			int length = 198;
@@ -66,7 +66,7 @@ namespace REQ001
 
 		}
 
-		TEST_METHOD(setWidthFunction_1)
+		TEST_METHOD(setWidthFunction1_WidthandInput_True)
 		{
 			//testing width function using width of 4 and input of 87  
 			int width = 4;
@@ -75,7 +75,7 @@ namespace REQ001
 			Assert::IsTrue(input == width);
 		}
 
-		TEST_METHOD(setWidthFunction_2)
+		TEST_METHOD(setWidthFunction2_WidthandInput_Notsame)
 		{
 			//testing width function by using width of 164 and input of 12 which are not equal
 			int width = 164;
@@ -84,7 +84,7 @@ namespace REQ001
 			Assert::AreNotSame(input, width);
 		}
 
-		TEST_METHOD(setWidthFunction_3)
+		TEST_METHOD(setWidthFunction3_WidthandInput_NotEqual)
 		{
 			//testing length function by using width of -27 and input of 187 which are not equal
 			int width = -27;
